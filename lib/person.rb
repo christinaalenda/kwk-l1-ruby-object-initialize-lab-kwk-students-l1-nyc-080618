e17a8b1486705f2563c5fb08c3f401fabc7cef7d
christinaalenda <clenda041@gmail.com>
2018-08-10 14:36:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+end
+
+new_name = Dog.new('tara')
+puts new_name.name
